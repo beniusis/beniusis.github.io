323ce1aafa0c4a5d3c21323c8240db088ceecea6
beniusis <rokasbeniu@gmail.com>
2024-05-23 20:16:42 +0300

    feat: create Header component

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ['Inter', 'sans-serif']
+      }
+    },
+    screens: {
+      xs: {
+        max: '480px'
+      }
+    }
+  },
+  plugins: []
+};
