ebf76b61451f64b5de33f6f5d6afec6b1234df17
beniusis <rokasbeniu@gmail.com>
2024-05-23 21:12:31 +0300

    impr: make the project link clickable on its full width

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -1,7 +1,7 @@
 export const Project = ({ data }) => {
   return (
     <a
-      className="flex flex-col justify-center items-start text-zinc-900 cursor-pointer hover:text-opacity-70 hover:-translate-x-1 hover:-translate-y-1 duration-300 ease-out xs:text-sm text-base"
+      className="flex flex-col justify-center items-start text-zinc-900 cursor-pointer hover:text-opacity-70 hover:-translate-x-1 hover:-translate-y-1 duration-300 ease-out xs:text-sm text-base w-full"
       href={data.url}
       target="_blank"
     >
