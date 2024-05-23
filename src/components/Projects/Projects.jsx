c93b79fc1f69160c3e928691d411caff4813c56d
beniusis <rokasbeniu@gmail.com>
2024-05-23 20:46:26 +0300

    ci: deploy to github pages

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -4,34 +4,34 @@ import { Project } from './Project';
 const PROJECTS = [
   {
     title: 'habitude',
-    description: 'A habit-tracking web application',
+    description: 'a habit-tracking web application',
     url: 'https://beniusis.github.io/habitude'
   },
   {
     title: 'gamefindr',
-    description: 'A simple game search web application',
+    description: 'a simple game search web application',
     url: 'https://beniusis.github.io/gamefindr'
   },
   {
     title: 'emojilicious',
-    description: 'A simple emoji search web application',
+    description: 'a simple emoji search web application',
     url: 'https://beniusis.github.io/emojilicious'
   },
   {
     title: 'favepix',
     description:
-      'A web application with infinite scroll functionality to search for images',
+      'a web application with infinite scroll functionality to search for images',
     url: 'https://beniusis.github.io/favepix'
   },
   {
     title: 'blitztype',
-    description: 'A web-based typing speed test application',
+    description: 'a web-based typing speed test application',
     url: 'https://beniusis.github.io/blitztype'
   },
   {
     title: 'diabetr',
     description:
-      'A tool that helps to calculate carbohydrates and insulin dosage for a better diabetes management on a daily basis',
+      'a tool that helps to calculate carbohydrates and insulin dosage for a better diabetes management on a daily basis',
     url: 'https://github.com/beniusis/diabetr'
   }
 ];
