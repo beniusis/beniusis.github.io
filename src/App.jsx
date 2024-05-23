c817c22fb75145fd68ae43320e99957e3ff44169
beniusis <rokasbeniu@gmail.com>
2024-05-23 21:05:15 +0300

    chore: change selection color

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { About, Header, Projects } from './components';
 
 export const App = () => {
   return (
-    <main className="max-w-2xl p-8 m-auto gap-10 flex flex-col min-h-screen font-inter justify-center items-center">
+    <main className="max-w-2xl p-8 m-auto gap-10 flex flex-col min-h-screen font-inter justify-center items-center selection:bg-zinc-400">
       <Header />
       <About />
       <Projects />
