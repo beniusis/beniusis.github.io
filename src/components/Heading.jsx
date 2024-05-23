5071b0e4557ef0c939b193ff029df443f6e5db85
beniusis <rokasbeniu@gmail.com>
2024-05-23 20:17:01 +0300

    feat: create Heading component

---
+++ src/components/Heading.jsx
@@ -0,0 +1,7 @@
+export const Heading = ({ children }) => {
+  return (
+    <h2 className="font-bold text-zinc-900 underline underline-offset-8 decoration-2 decoration-indigo-400">
+      {children}
+    </h2>
+  );
+};
