bf30e8709f81c2c5009600d5829ef9daf3d56c4d
beniusis <rokasbeniu@gmail.com>
2024-05-24 17:01:36 +0300

    impr: add repo url to projects

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -5,28 +5,33 @@ const PROJECTS = [
   {
     title: 'habitude',
     description: 'a habit-tracking web application',
-    url: 'https://beniusis.github.io/habitude'
+    url: 'https://beniusis.github.io/habitude',
+    repo: 'https://github.com/beniusis/habitude'
   },
   {
     title: 'gamefindr',
     description: 'a simple game search web application',
-    url: 'https://beniusis.github.io/gamefindr'
+    url: 'https://beniusis.github.io/gamefindr',
+    repo: 'https://github.com/beniusis/gamefindr'
   },
   {
     title: 'emojilicious',
     description: 'a simple emoji search web application',
-    url: 'https://beniusis.github.io/emojilicious'
+    url: 'https://beniusis.github.io/emojilicious',
+    repo: 'https://github.com/beniusis/emojilicious'
   },
   {
     title: 'favepix',
     description:
       'a web application with infinite scroll functionality to search for images',
-    url: 'https://beniusis.github.io/favepix'
+    url: 'https://beniusis.github.io/favepix',
+    repo: 'https://github.com/beniusis/favepix'
   },
   {
     title: 'blitztype',
     description: 'a web-based typing speed test application',
-    url: 'https://beniusis.github.io/blitztype'
+    url: 'https://beniusis.github.io/blitztype',
+    repo: 'https://github.com/beniusis/blitztype'
   },
   {
     title: 'diabetr',
