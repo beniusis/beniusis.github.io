8ee390d653467f409e664a307822e6681684bbdb
beniusis <rokasbeniu@gmail.com>
2024-06-15 23:56:27 +0300

    chore: update resume

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { About, Header, Projects } from './components';
 
 export const App = () => {
   return (
-    <div className="m-auto min-h-screen max-w-2xl p-8 selection:bg-zinc-400">
+    <div className="m-auto min-h-screen max-w-2xl p-8 selection:bg-zinc-50">
       <Header />
       <main className="mt-10 flex flex-col items-center justify-center gap-10">
         <About />
