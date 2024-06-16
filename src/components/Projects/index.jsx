a5af5ade23112646226f334a70beb578cf8a70a5
beniusis <rokasbeniu@gmail.com>
2024-06-16 10:07:57 +0300

    chore: update projects links

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -1,50 +1,6 @@
 import { Heading } from '@/components';
 import { Project } from './Project';
 
-const PROJECTS = [
-  {
-    title: 'habitude',
-    description:
-      'a habit-tracking web application that helps you stick to new daily habits (working out, reading, meditating, etc.)',
-    url: 'https://beniusis.github.io/habitude',
-    repo: 'https://github.com/beniusis/habitude'
-  },
-  {
-    title: 'gamefindr',
-    description:
-      'a simple web application for searching video games and basic information about them (minimum requirements, release date, etc.)',
-    url: 'https://beniusis.github.io/gamefindr',
-    repo: 'https://github.com/beniusis/gamefindr'
-  },
-  {
-    title: 'emojilicious',
-    description:
-      'a simple emoji search web application with the ability to search by keywords and copy emojis to the clipboard',
-    url: 'https://beniusis.github.io/emojilicious',
-    repo: 'https://github.com/beniusis/emojilicious'
-  },
-  {
-    title: 'favepix',
-    description:
-      'an infinite scroll-capable web application for viewing the latest images from the Pexels API and saving your favorite ones',
-    url: 'https://beniusis.github.io/favepix',
-    repo: 'https://github.com/beniusis/favepix'
-  },
-  {
-    title: 'blitztype',
-    description: 'a minimalistic typing speed test web application',
-    url: 'https://beniusis.github.io/blitztype',
-    repo: 'https://github.com/beniusis/blitztype'
-  },
-  {
-    title: 'diabetr',
-    description:
-      "a command-line interface application that helps with meal's carbohydrates, insulin dose calculation and tracks insulin injections",
-    url: 'https://github.com/beniusis/diabetr',
-    repo: 'https://github.com/beniusis/diabetr'
-  }
-];
-
 export const Projects = () => {
   return (
     <div className="flex flex-col gap-6">
@@ -58,3 +14,47 @@ export const Projects = () => {
     </div>
   );
 };
+
+const PROJECTS = [
+  {
+    title: 'habitude',
+    description:
+      'a habit-tracking web application that helps you stick to new daily habits (working out, reading, meditating, etc.)',
+    url: 'https://beniusis.dev/habitude',
+    repo: 'https://github.com/beniusis/habitude'
+  },
+  {
+    title: 'gamefindr',
+    description:
+      'a simple web application for searching video games and basic information about them (minimum requirements, release date, etc.)',
+    url: 'https://beniusis.dev/gamefindr',
+    repo: 'https://github.com/beniusis/gamefindr'
+  },
+  {
+    title: 'emojilicious',
+    description:
+      'a simple emoji search web application with the ability to search by keywords and copy emojis to the clipboard',
+    url: 'https://beniusis.dev/emojilicious',
+    repo: 'https://github.com/beniusis/emojilicious'
+  },
+  {
+    title: 'favepix',
+    description:
+      'an infinite scroll-capable web application for viewing the latest images from the Pexels API and saving your favorite ones',
+    url: 'https://beniusis.dev/favepix',
+    repo: 'https://github.com/beniusis/favepix'
+  },
+  {
+    title: 'blitztype',
+    description: 'a minimalistic typing speed test web application',
+    url: 'https://beniusis.dev/blitztype',
+    repo: 'https://github.com/beniusis/blitztype'
+  },
+  {
+    title: 'diabetr',
+    description:
+      "a command-line interface application that helps with meal's carbohydrates, insulin dose calculation and tracks insulin injections",
+    url: 'https://github.com/beniusis/diabetr',
+    repo: 'https://github.com/beniusis/diabetr'
+  }
+];
