be42e7b756e8be225f830f77ef9598eebe396ee8
beniusis <rokasbeniu@gmail.com>
2024-06-28 21:28:48 +0300

    chore: change about section text

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -8,7 +8,7 @@ export const About = () => {
     <div className="flex flex-col items-start justify-center gap-6">
       <Heading>About</Heading>
 
-      <div className="flex flex-col gap-4 text-pretty text-left text-base font-normal text-zinc-900 xs:text-sm">
+      <div className="flex flex-col gap-4 text-balance text-left text-base font-normal text-zinc-900 xs:text-sm">
         <p>
           A curious and ambitious software developer with a passion for web development and a focus
           on front-end development. I am committed to building user-friendly, effective, and
@@ -17,11 +17,10 @@ export const About = () => {
         </p>
         <p>
           I am enrolled in the course of Web Development at{' '}
-          <Link href="https://www.turingcollege.com">Turing College</Link> and looking for new
-          opportunities and challenges to grow, apply, and further develop my skills in the
-          professional setting.
+          <Link href="https://www.turingcollege.com">Turing College</Link>.
+          <br />
+          Currently learning: {CURRENTLY_LEARNING.join(', ')}.
         </p>
-        <p>Currently learning: {CURRENTLY_LEARNING.join(', ')}.</p>
       </div>
     </div>
   );
